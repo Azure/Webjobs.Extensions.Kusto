6f08fac962843d22c8e3138942d3ee19be0feb18
Ramachandran G <ramacg@microsoft.com>
2023-01-25 21:50:23 +0530

    * Get out-of-process samples to work

--- samples/samples-outofproc/Common/ProductUtilities.cs
+++ samples/samples-outofproc/Common/ProductUtilities.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
-using System;
-using System.Collections.Generic;
 using Microsoft.Azure.WebJobs.Extensions.Kusto.SamplesOutOfProc.OutputBindingSamples.Common;
 
 namespace Microsoft.Azure.WebJobs.Extensions.Kusto.Samples.Common
--- samples/samples-outofproc/Program.cs
+++ samples/samples-outofproc/Program.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
 using Microsoft.Extensions.Hosting;
@@ -9,8 +9,8 @@ namespace Microsoft.Azure.WebJobs.Extensions.Kusto.SamplesOutOfProc
         public static void Main()
         {
             IHost host = new HostBuilder()
-                .ConfigureFunctionsWorkerDefaults()
-                .Build();
+    .ConfigureFunctionsWorkerDefaults()
+    .Build();
 
             host.Run();
         }
