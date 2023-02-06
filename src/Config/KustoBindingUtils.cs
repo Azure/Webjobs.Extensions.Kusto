24d963f314d0cd69608e3fc22d4cb71ccc20d5b5
Ramachandran A G <106139410+ag-ramachandran@users.noreply.github.com>
2023-02-06 13:22:45 +0530

    Bugfix/add loggers and additional tests (#13)
    * Add logs with exception handling while parsing connection string
    * Use ConnectionString parser to sanitize strings with keys
    * Add additional logs
    * Add additional tests for E2E scenarios
    * Add set up instructions
    * Add keys for installation

---
+++ src/Config/KustoBindingUtils.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+namespace Microsoft.Azure.WebJobs.Extensions.Kusto.Config
+{
+    internal class KustoBindingUtils
+    {
+        /// <summary>
+        /// Given a KustoConnectionString, scrubs out sensitive parts of the string and returns a sanitized string
+        /// </summary>
+        /// <param name="KustoConnectionString"></param>
+        /// <returns>A sanitized string that removes the sensisitive token from Kusto connection string</returns>
+        internal static string ToSecureString(string KustoConnectionString)
+        {
+            var builder = new System.Data.Common.DbConnectionStringBuilder
+            {
+                ConnectionString = KustoConnectionString
+            };
+
+            string[] securityQualifiers = new string[] {
+                "AppKey", "Application Key", "ApplicationKey",
+                "UserID", "User ID", "UID", "User",
+                "User Token", "UsrToken", "UserToken", "UserToken",
+                "Application Token", "AppToken", "ApplicationToken"
+            };
+            foreach (string qualifier in securityQualifiers)
+            {
+                if (builder.ContainsKey(qualifier))
+                {
+                    builder[qualifier] = "********";
+                }
+            }
+            return builder.ConnectionString;
+        }
+    }
+}
---
+++ test/Config/KustoBindingUtilsTests.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
+using Microsoft.Azure.WebJobs.Extensions.Kusto.Config;
+using Xunit;
+
+namespace Microsoft.Azure.WebJobs.Extensions.Kusto.Tests.Config
+{
+    public class KustoBindingUtilsTests
+    {
+        [Theory]
+        [InlineData
+            ("Data Source=https://randomcluster.eastus.dev.kusto.windows.net;Database=testdb;Fed=True;AppClientId=214dd011-44fd-4893-fc2f-6f6c3c85b90c;AppKey=areallyfakekey;Authority Id=11f111bf-11f2-11af-11ab-1d1cd111db11",
+             "Data Source=https://randomcluster.eastus.dev.kusto.windows.net;Database=testdb;Fed=True;AppClientId=214dd011-44fd-4893-fc2f-6f6c3c85b90c;AppKey=********;Authority Id=11f111bf-11f2-11af-11ab-1d1cd111db11")]
+        [InlineData("Data Source=https://randomcluster.eastus.dev.kusto.windows.net;Database=testdb;Fed=True;User ID=UserId;AppKey=areallyfakekey!;Authority Id=11f111bf-11f2-11af-11ab-1d1cd111db11;Application Token=apptoken11",
+             "Data Source=https://randomcluster.eastus.dev.kusto.windows.net;Database=testdb;Fed=True;User ID=********;AppKey=********;Authority Id=11f111bf-11f2-11af-11ab-1d1cd111db11;Application Token=********")]
+        [InlineData("Data Source=https://randomcluster.eastus.dev.kusto.windows.net;Database=testdb;Fed=True",
+             "Data Source=https://randomcluster.eastus.dev.kusto.windows.net;Database=testdb;Fed=True")]
+        public void ToSecureStringTest(string connectionStringInput, string expectedString)
+        {
+            string actualString = KustoBindingUtils.ToSecureString(connectionStringInput);
+            Assert.True(string.Equals(expectedString, actualString, StringComparison.OrdinalIgnoreCase));
+        }
+    }
+}
