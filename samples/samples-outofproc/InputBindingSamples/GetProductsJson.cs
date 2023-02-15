42773c7aa580cbd0cd16d54372b3ad28f888a78e
Ramachandran G <ramacg@microsoft.com>
2023-02-15 22:15:27 +0530

    *Fix conflicting names in Integration tests

--- samples/samples-outofproc/InputBindingSamples/GetProductsJson.cs
+++ samples/samples-outofproc/InputBindingSamples/GetProductsJson.cs
@@ -10,7 +10,7 @@ namespace Microsoft.Azure.WebJobs.Extensions.Kusto.SamplesOutOfProc.InputBinding
 {
     public static class GetProductsJson
     {
-        [Function("GetProducts")]
+        [Function("GetProductsJson")]
         public static Task<List<Product>> Run(
 #pragma warning disable 
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "getproductsjson/{productId}")] HttpRequestData req,
