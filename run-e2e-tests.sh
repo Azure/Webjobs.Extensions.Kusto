ff48a00e568ea3d5c27de66abdee97281d6c2905
ag-ramachandran <ramacg@microsoft.com>
2025-06-30 12:34:40 +0530

    change to webjobs db

--- run-e2e-tests.sh
+++ run-e2e-tests.sh
@@ -9,7 +9,7 @@ TestClusterName=$1
 echo "--- Setting up environment variables ---"
 UserAccessToken=`az account get-access-token --scope "$TestClusterName/.default" --query accessToken -o tsv`
 export KustoConnectionString="Data Source=$TestClusterName;Database=e2e;Fed=True;UserToken=$UserAccessToken"
-export KustoConnectionStringNoPermissions="Data Source=$TestClusterName;Database=e2e_secondary;Fed=True;UserToken=$UserAccessToken"
+export KustoConnectionStringNoPermissions="Data Source=$TestClusterName;Database=webjobs;Fed=True;UserToken=$UserAccessToken"
 export KustoConnectionStringMSI="Data Source=$TestClusterName;Database=e2e;Fed=True;"
 export KustoConnectionStringInvalidAttributes="Data Source=$TestClusterName;Database=e2e;Fed=True;AppClientId=72f988bf-86f1-41af-91ab-2d7cd011db47"
 echo "--- Setting up dotnet env ---"
